f67a8e3ee1b3f66163328951f8994b7a60c6961d
GitPyVKJ <vkjoharwal@gmail.com>
2025-11-27 23:19:04 +0530

    Create PyAssign1.py

---
+++ Tutedude/PyAssign1.py
@@ -0,0 +1,12 @@
+"""Task One"""
+"""x=int(input("Enter First  Number 'x': "))
+y=int(input("Enter second Number 'y': "))
+print("Sum x+y=",(x+y))
+print("Subtraction x-y=",(x-y))
+print("Multiple x*y=",(x*y))
+print("Division x/y=",(x/y))"""
+
+"""Task Two"""
+x=input("Enter First Name: ")
+y=input("Enter Last  Name : ")
+print("Hello, ",x,y,"! Welcome to my Pythone Programme.")
